69e6880ffd8e62c64f12641b1db11c604dc585b0
AdelinaVilevna <adelina.abdykanyeva@gmail.com>
2022-06-12 12:08:31 -1000

    Java Programming day 23

--- src/day23_ArrayList/RemoveDup.java
+++ src/day23_ArrayList/RemoveDup.java
@@ -1,5 +1,6 @@
 package day23_ArrayList;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class RemoveDup {
@@ -27,6 +28,59 @@ public class RemoveDup {
         names = nonDup;
 
         System.out.println(names);
+
+
+        ArrayList<Integer>list1 = new ArrayList<>();
+        ArrayList<Integer>list2 =list1;
+
+        list1.add(10);
+        System.out.println(list1);
+        System.out.println(list2);
+
+        System.out.println(list1 == list2);
+
+        ArrayList<Integer> l1 = new ArrayList<>();
+
+        ArrayList<Integer>l2 = new ArrayList<>();
+
+        l1.add(10);
+        l2.add(10);
+        System.out.println(l1);
+        System.out.println(l2);
+
+        System.out.println(l1 == l2);
+
+        System.out.println(l1.equals(l2));
+
+
+        System.out.println("___________________________________");
+
+        ArrayList<Integer>n1 = new ArrayList<>();
+        n1.add(10);
+        n1.add(20);
+        n1.add(30);
+
+
+        ArrayList<Integer>n2 = new ArrayList<>();
+        n2.add(30);
+        n2.add(20);
+        n2.add(10);
+
+
+        System.out.println(n1.equals(n2));
+
+
+        System.out.println(n1.isEmpty());
+        n1.clear();
+
+        System.out.println(n1.isEmpty());
+
+
+
+
+
+
+
 
     }
 }
