5faf7e93865253949173c76bc9190be2e7f307ec
AdelinaVilevna <adelina.abdykanyeva@gmail.com>
2022-06-16 10:39:46 -1000

    Java Programming day 24

---
+++ src/day23_ArrayList/JadenCasing.java
@@ -0,0 +1,19 @@
+package day23_ArrayList;
+
+public class JadenCasing {
+
+    public static void main(String[] args) {
+
+
+        String str = "How can mirrors be real if our eyes aren't real";
+
+        String[] jadenCased = str.split(" ");
+        String result = "";
+
+        for (int i = 0; i < jadenCased.length; i++) {
+            result += jadenCased[i].substring(0, 1).toUpperCase() + jadenCased[i].substring(1) + " ";
+        }
+        System.out.println(result);
+
+    }
+}
