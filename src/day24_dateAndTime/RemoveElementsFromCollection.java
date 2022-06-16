294ea3068f432ca2aa33b2b62026691a3fa3815e
AdelinaVilevna <adelina.abdykanyeva@gmail.com>
2022-06-16 11:41:50 -1000

    Java Programming day 24

--- src/day24_dateAndTime/RemoveElementsFromCollection.java
+++ src/day24_dateAndTime/RemoveElementsFromCollection.java
@@ -1,5 +1,7 @@
 package day24_dateAndTime;
 
+import day17_customClass.Employee;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -26,6 +28,35 @@ public class RemoveElementsFromCollection {
 
         list2.removeIf( p -> p < 4);
         System.out.println(list2);
+
+        System.out.println("_________________________________________________");
+
+
+        ArrayList<String> names = new ArrayList<>();
+        names.addAll(Arrays.asList("Java", "Python", "C#", "Ruby", "Javascript", "C++", "C"));
+
+        names.removeIf(p -> p.startsWith("J"));
+        System.out.println(names);
+
+        System.out.println("_________________________________________________");
+
+        ArrayList<Employee> employees = new ArrayList<>();
+        employees.addAll(Arrays.asList(
+                new Employee(), new Employee(), new Employee(), new Employee(), new Employee()));
+
+        employees.get(0).setInfo("Josh", 34, 'M', "Java Developer", 100000, "A01");
+        employees.get(1).setInfo("Emily", 40, 'F', "SDET", 90000, "A02");
+        employees.get(2).setInfo("Conor", 38, 'M', "Project Manager", 130000, "A03");
+        employees.get(3).setInfo("Bella", 29, 'F', "Java Developer", 95000, "A04");
+        employees.get(4).setInfo("Jimmy", 54, 'M', "Data Analyst", 105000, "A05");
+
+
+        employees.removeIf(each -> each.salary > 100000);
+
+        for(Employee each : employees){
+            System.out.println(each.name + ": " + each.salary);
+        }
+
 
 
 
