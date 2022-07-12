1888435a34cb710f677c1c1ca9ab4b376dc75bed
adelinaabdykanyeva <adelina.abdykanyeva@gmail.com>
2022-07-12 14:43:31 -0500

    Java Programming day 38

--- src/day38_exceptions/shape/Circle.java
+++ src/day38_exceptions/shape/Circle.java
@@ -1,4 +1,23 @@
 package day38_exceptions.shape;
 
 public class Circle {
+
+    private double radius;
+
+
+    public double getRadius() {
+       return radius;
+    }
+
+    public void setRadius(double radius) {
+        if(radius <= 0){
+            throw new InvalidShapeException("Radius of the circle can not be negative or zero: " + radius);
+        }
+        this.radius = radius;
+    }
+
+    public Circle(double radius) {
+        setRadius(radius);
+
+    }
 }
