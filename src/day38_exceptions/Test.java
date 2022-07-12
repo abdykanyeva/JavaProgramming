44cfdef37afd6399ad3dff0511b36207f2be33e1
adelinaabdykanyeva <adelina.abdykanyeva@gmail.com>
2022-07-12 12:55:37 -0500

    Java Programming day 38

---
+++ src/day38_exceptions/Test.java
@@ -0,0 +1,17 @@
+package day38_exceptions;
+
+import utilities.Library;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello Cydeo");
+
+      //  Thread.sleep(35000);
+        Library.sleep(3.5);
+
+        System.out.println("How are you today?");
+    }
+}
--- src/day38_exceptions/ThrowsKeyword2.java
+++ src/day38_exceptions/ThrowsKeyword2.java
@@ -1,5 +1,8 @@
 package day38_exceptions;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class ThrowsKeyword2 {
 
     public static void main(String[] args) throws InterruptedException {
@@ -20,6 +23,18 @@ public class ThrowsKeyword2 {
         System.out.println("Hello World");
         pauseFor5Seconds();
         System.out.println("Hello Cydeo");
+    }
+
+    public static void method2() throws InterruptedException, FileNotFoundException {
+        System.out.println("First program started");
+
+        Thread.sleep(3000);
+
+        System.out.println("First program ended");
+
+        new FileInputStream(" ");
+
+        Thread.sleep(5000);
     }
 
 
