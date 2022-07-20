580436edff94f492767a1d1afcf52f369c8831d1
adelinaabdykanyeva <adelina.abdykanyeva@gmail.com>
2022-07-20 12:34:56 -0500

    Java Programming day 42

--- src/day42_maps/MapPractice5.java
+++ src/day42_maps/MapPractice5.java
@@ -1,8 +1,22 @@
 package day42_maps;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 public class MapPractice5 {
 
     public static void main(String[] args) {
+
+
+
+        Map<String, String> map = new TreeMap<>();
+
+        map.put("C02", "Josh");
+        map.put("B02", "Nora");
+        map.put("A02", "Kosh");
+        map.put("D02", "Dosh");
+
+        map.forEach( (k, v) -> {System.out.println(k +": " + v); });
 
 
 
